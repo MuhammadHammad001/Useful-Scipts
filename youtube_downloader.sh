ab1bc512d1337b77b13225e6f6125024adc3e20c
MuhammadHammad001 <hammad.bashir@10xengineers.ai>
2024-01-17 17:41:38 +0500

    Script for running Yotube-Downloader-application added

---
+++ youtube_downloader.sh
@@ -0,0 +1,11 @@
+# Script for Youtube Downloader App
+# Copyright (c) 2024 Muhammad Hammad Bashir
+
+# This application allows users to download YouTube videos.
+
+# Created by Muhammad Hammad Bashir
+
+#!/bin/bash
+#This script is used to run the Python based Youtube Videos Downloader application.
+
+python3 Youtube-video-downloader/main.py
